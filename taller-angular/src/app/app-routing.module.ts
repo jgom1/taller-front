60c53cc830bd026b63d1d43352475ffd48b2d9f0
Gom <email>
2020-09-10 13:48:03 +0200

    Added initial pages routes

--- taller-angular/src/app/app-routing.module.ts
+++ taller-angular/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListPageComponent } from './pages/product-list-page/product-list-page.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { FavouritesPageComponent } from './pages/favourites-page/favourites-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { PurchasesPageComponent } from './pages/purchases-page/purchases-page.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductListPageComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'favorites', component: FavouritesPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'purchases', component: PurchasesPageComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', component: ProductListPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
