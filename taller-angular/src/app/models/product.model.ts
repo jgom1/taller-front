d47e3541505b3ffb2a4c6298a7597c7668fa9513
Gom <email>
2020-09-14 08:17:33 +0200

    Created product and user models

---
+++ taller-angular/src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    "id"?: number;
+    "productName"?: string;
+    "productResume"?: string;
+    "productPromotion"?: boolean;
+    "productOldPrice"?: number;
+    "productPrice"?: number;
+    "productQuantity"?: number;
+    "productImage"?: string;
+}
---
+++ taller-angular/src/app/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface User {
+    "id"?: number;
+    "userName"?: string;
+    "userSurname"?: string;
+    "userEmail"?: string;
+    "userPassword"?: string;
+}
