c42d6339a76f9a9559d1fff8a6a8eab8194cb1b8
Gom <email>
2020-09-15 19:38:43 +0200

    Fixed problem in login component

--- taller-angular/src/app/components/login/login.component.ts
+++ taller-angular/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { appActions } from '../../store/app.actions';
 import { UserService } from '../../services/user.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -13,7 +13,6 @@ import { appState } from '../../store/app.state.interface';
 })
 export class LoginComponent implements OnInit, OnDestroy {
 
-  private logged$: Observable<boolean> = this.store.select((state: any) => state.app.userLogged);
   public loginForm: FormGroup;
   public errorMessage: string;
   private subscription: Subscription = new Subscription();
@@ -24,34 +23,11 @@ export class LoginComponent implements OnInit, OnDestroy {
     private store: Store<appState>) { }
 
   ngOnInit(): void {
-    this.subscribeLogged();
     this.createForm();
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
-  }
-
-  public login() {
-    this.subscription.add(
-      this.userService.getUserByEmail(this.loginForm.get('email').value).subscribe((data: any) => {
-        if (data.length == 0 || data[0].userPassword !== this.loginForm.get('password').value) {
-          this.errorMessage = 'El usuario introducido no existe o los datos no son correctos.'
-        } else {
-          this.solveSuccessfulLogin(data[0]);
-        }
-      })
-    );
-  }
-
-  private subscribeLogged(): void {
-    this.subscription.add(
-      this.logged$.subscribe((data: any) => {
-        if (data === true) {
-          document.getElementById('closeModal').click();
-        }
-      })
-    );
   }
 
   private createForm(): void {
@@ -67,6 +43,19 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.errorMessage = '';
     this.store.dispatch(appActions.login());
     this.store.dispatch(appActions.setUser(user));
+    document.getElementById('closeLoginModal').click();
+  }
+
+  public login(): void {
+    this.subscription.add(
+      this.userService.getUserByEmail(this.loginForm.get('email').value).subscribe((data: any) => {
+        if (data.length == 0 || data[0].userPassword !== this.loginForm.get('password').value) {
+          this.errorMessage = 'El usuario introducido no existe o los datos no son correctos.'
+        } else {
+          this.solveSuccessfulLogin(data[0]);
+        }
+      })
+    );
   }
 
 }
