c9a98c7d7564273e007cd54db00d1e06f62b5b85
Gom <email>
2020-09-23 17:35:37 +0200

    Installed react-hook-form and added form logic to Login component

--- taller-react/src/app/components/header/Login.js
+++ taller-react/src/app/components/header/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
 
 /* Bootstrap imports */
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,9 +7,32 @@ import Modal from 'react-bootstrap/Modal';
 
 export const Login = () => {
     const [showLoginModal, setShowLoginModal] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const { register, handleSubmit, formState } = useForm({ mode: "onChange" });
 
     const handleCloseLoginModal = () => setShowLoginModal(false);
     const handleShowLoginModal = () => setShowLoginModal(true);
+
+    async function fetchUser(formData) {
+        const res = await fetch(`http://localhost:3004/users?userEmail=${formData.loginEmail}`);
+        res.json().then(res => {
+            if (res.length > 0) {
+                setErrorMessage('');
+                if (res[0].userPassword === formData.loginPassword) {
+                    console.log('Entrar');
+                } else {
+                    setErrorMessage('Los datos introducidos no son correctos.');
+                }
+            } else {
+                setErrorMessage('No existe nigún usuario con ese correo electrónico.');
+            }
+        });
+    }
+
+    const loginSubmit = (formData) => {
+        fetchUser(formData);
+    }
+
 
     return (
         <React.Fragment>
@@ -16,20 +40,22 @@ export const Login = () => {
             <Modal show={showLoginModal} onHide={handleCloseLoginModal}>
                 <Modal.Body className="p-5 text-center">
                     <h2 className="mb-5">¡Bienvenido!</h2>
-                    <div className="form-group mt-5">
-                        <label htmlFor="email" className="mb-1 h5">Correo electrónico</label>
-                        <input type="email" className="form-control" id="loginEmail" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="mb-1 h5">Contraseña</label>
-                        <input type="password" className="form-control" id="loginPassword" required />
-                        <small className="form-text text-danger">Mensaje de error</small>
-                    </div>
-                    <div className="row m-0 mt-5 justify-content-center">
-                        <button type="button" className="col-4 btn btn-outline-danger mr-2"
-                            id="closeLoginModal" onClick={handleCloseLoginModal}>Cerrar</button>
-                        <button type="submit" className="col-4 btn btn-dark ml-2">Entrar</button>
-                    </div>
+                    <form onSubmit={handleSubmit(loginSubmit)}>
+                        <div className="form-group mt-5">
+                            <label htmlFor="email" className="mb-1 h5">Correo electrónico</label>
+                            <input type="email" className="form-control" name="loginEmail" id="loginEmail" ref={register({ required: true })} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password" className="mb-1 h5">Contraseña</label>
+                            <input type="password" className="form-control" name="loginPassword" id="loginPassword" ref={register({ required: true })} />
+                        </div>
+                        {errorMessage && <p class="mb-0 text-danger">{errorMessage}</p>}
+                        <div className="row m-0 mt-5 justify-content-center">
+                            <button type="button" className="col-4 btn btn-outline-danger mr-2"
+                                id="closeLoginModal" onClick={handleCloseLoginModal}>Cerrar</button>
+                            <button type="submit" disabled={!formState.isValid} className="col-4 btn btn-dark ml-2">Entrar</button>
+                        </div>
+                    </form>
                 </Modal.Body>
             </Modal>
         </React.Fragment>
