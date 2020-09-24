cf3e091b61f1dacca367e53cad51425735e9747d
Gom <email>
2020-09-24 09:14:22 +0200

    Created ShoppingCart component initial layout from Header component

--- taller-react/src/app/components/header/Header.js
+++ taller-react/src/app/components/header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
-import { selectLogged, selectUser } from '../../../features/counter/counterSlice';
+import { selectLogged } from '../../../features/counter/counterSlice';
 
 /* Bootstrap imports */
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Badge from 'react-bootstrap/Badge';
 
 /* Children components */
 import { Registration } from './Registration';
 import { Login } from './Login';
 import { NavDropdown } from './NavDropdown';
+import { ShoppingCart } from './ShoppingCart';
 
 
 const UnloggedNav = () => {
@@ -23,18 +23,10 @@ const UnloggedNav = () => {
 };
 
 const LoggedNav = () => {
-    
     return (
         <div className="d-flex align-items-center text-white">
-           <NavDropdown /> 
-            <div className="d-flex align-items-center cursor-pointer">
-                <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-cart-fill ml-2 mb-0"
-                    fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                </svg>
-                <Badge className="align-self-start" variant="light">0</Badge>
-            </div >
+            <NavDropdown />
+            <ShoppingCart />
         </div >
     )
 };
