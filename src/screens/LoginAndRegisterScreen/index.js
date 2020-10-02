9db0edc6873217f2b04fe203f90395fdb4b4aa08
Gom <email>
2020-10-02 14:59:24 +0200

    Installed axios and json-server libraries and added logic to LoginAndRegister component

--- src/screens/LoginAndRegisterScreen/index.js
+++ src/screens/LoginAndRegisterScreen/index.js
@@ -1,16 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
-import { Icon, Text, Button, Input } from 'react-native-elements';
+import { Text, Input } from 'react-native-elements';
+import axios from 'axios';
 
 export const LoginAndRegisterScreen = ({ navigation }) => {
+    
     const [showLogin, setShowLogin] = useState(true);
+    const [dataLogin, setDataLogin] = useState({ user: '', password: '' });
+    const [errorLogin, setErrorLogin] = useState('');
+    
     const [showRegistration, setShowRegistration] = useState(false);
+    const [dataRegistration, setDataRegistration] = useState({ name: '', surname: '', email: '', password: '', confirmPassword: '' });
+    const [errorRegistration, setErrorRegistration] = useState('');
+
+    const submitLogin = () => {
+        if (showLogin && dataLogin.user && dataLogin.password) {
+            axios.get(`http://10.0.2.2:3004/users?userEmail=${(dataLogin.user).trim().toLowerCase()}`)
+                .then(function (response) {
+                    if (response.data.length === 0 || response.data[0].userPassword !== dataLogin.password) {
+                        setErrorLogin('El correo o la contraseña introducidos no son correctos');
+                    } else {
+                        setErrorLogin('');
+                        navigation.navigate('Productos');
+                    }
+                })
+                .catch(function (error) { })
+        } else {
+            setShowLogin(true);
+            setShowRegistration(false);
+        }
+    }
+
+    const saveNewUser = () => {
+        const headers = {
+            "Content-type": "application/json; charset=UTF-8"
+        };
+        const newUser = {
+            'userName': dataRegistration.name,
+            'userSurname': dataRegistration.surname,
+            'userEmail': dataRegistration.email,
+            'userPassword': dataRegistration.password
+        }
+        axios.post('http://10.0.2.2:3004/users', newUser, headers)
+            .then(function (response) {
+                navigation.navigate('Productos');
+            })
+            .catch(function (error) { })
+    }
+
+    const checkNewUserEmail = () => {
+        axios.get(`http://10.0.2.2:3004/users?userEmail=${(dataRegistration.email).trim().toLowerCase()}`)
+            .then(function (response) {
+                if (response.data.length === 0) {
+                    saveNewUser();
+                } else {
+                    setErrorRegistration('Ya existe un usuario con ese correo');
+                }
+            })
+            .catch(function (error) { })
+    }
+
+    const submitRegistration = () => {
+        if (showRegistration && dataRegistration.name && dataRegistration.surname
+            && dataRegistration.email && dataRegistration.password
+            && dataRegistration.confirmPassword) {
+            if (dataRegistration.password === dataRegistration.confirmPassword) {
+                checkNewUserEmail();
+            } else {
+                setErrorRegistration('Las contraseñas introducidas deben coincidir');
+            }
+        } else {
+            setShowLogin(false);
+            setShowRegistration(true);
+        }
+    }
 
     return (
-        <ScrollView  style={styles.landing__container}>
+        <ScrollView style={styles.landing__container}>
             <View style={styles.landing__welcomeText}>
                 <Text h4>Taller&nbsp;
-                    <Text style={styles.landing__logoColor}>&nbsp;React&nbsp;</Text>
+                    <Text style={styles.landing__logoColor}>&nbsp;React native&nbsp;</Text>
                 </Text>
                 <Text h2>¡Bienvenido!</Text>
             </View>
@@ -20,35 +89,57 @@ export const LoginAndRegisterScreen = ({ navigation }) => {
                         <Input
                             placeholder='Usuario'
                             leftIcon={{ type: 'font-awesome', name: 'user' }}
+                            onEndEditing={(event) => setDataLogin((prevData) => ({ ...prevData, user: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorLogin('')}
                         />
                         <Input
                             placeholder='Contraseña'
+                            secureTextEntry
                             leftIcon={{ type: 'font-awesome', name: 'unlock-alt' }}
+                            onEndEditing={(event) => setDataLogin((prevData) => ({ ...prevData, password: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorLogin('')}
                         />
+                        <Text style={styles.landing__errorMessage}>{errorLogin}</Text>
                     </View>
                 }
                 {showRegistration &&
                     <View style={styles.landing__dataInput__registrationContainer}>
                         <Input
                             style={styles.landing__dataInput__registrationInputs}
-                            placeholder='Nombre y apellidos'
+                            placeholder='Nombre'
                             leftIcon={{ type: 'font-awesome', name: 'user' }}
+                            onEndEditing={(event) => setDataRegistration((prevData) => ({ ...prevData, name: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorRegistration('')}
+                        />
+                        <Input
+                            style={styles.landing__dataInput__registrationInputs}
+                            placeholder='Apellidos'
+                            leftIcon={{ type: 'font-awesome', name: 'user' }}
+                            onEndEditing={(event) => setDataRegistration((prevData) => ({ ...prevData, surname: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorRegistration('')}
                         />
                         <Input
                             style={styles.landing__dataInput__registrationInputs}
                             placeholder='Correo electrónico'
                             leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+                            onEndEditing={(event) => setDataRegistration((prevData) => ({ ...prevData, email: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorRegistration('')}
                         />
                         <Input
                             style={styles.landing__dataInput__registrationInputs}
                             placeholder='Contraseña'
                             leftIcon={{ type: 'font-awesome', name: 'unlock-alt' }}
+                            onEndEditing={(event) => setDataRegistration((prevData) => ({ ...prevData, password: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorRegistration('')}
                         />
                         <Input
                             style={styles.landing__dataInput__registrationInputs}
                             placeholder='Confirma la contraseña'
                             leftIcon={{ type: 'font-awesome', name: 'unlock-alt' }}
+                            onEndEditing={(event) => setDataRegistration((prevData) => ({ ...prevData, confirmPassword: event.nativeEvent.text || '' }))}
+                            onFocus={() => setErrorRegistration('')}
                         />
+                        <Text style={styles.landing__errorMessage}>{errorRegistration}</Text>
                     </View>
                 }
             </View>
@@ -56,7 +147,7 @@ export const LoginAndRegisterScreen = ({ navigation }) => {
                 <View style={styles.landing__buttonRegistration}>
                     <TouchableOpacity
                         style={styles.landing__buttonRegistration__button}
-                        onPress={() => { setShowLogin(false); setShowRegistration(true) }}
+                        onPress={submitRegistration}
                     >
                         <Text h4 style={styles.landing__buttonRegistration__text}>Registrarme</Text>
                     </TouchableOpacity>
@@ -64,7 +155,7 @@ export const LoginAndRegisterScreen = ({ navigation }) => {
                 <View style={styles.landing__buttonLogin}>
                     <TouchableOpacity
                         style={styles.landing__buttonLogin__button}
-                        onPress={() => navigation.navigate('Productos')}
+                        onPress={submitLogin}
                     >
                         <Text h4 style={styles.landing__buttonLogin__text}>Entrar</Text>
                     </TouchableOpacity>
@@ -77,12 +168,10 @@ export const LoginAndRegisterScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
     landing__container: {
-        flex: 1,
         backgroundColor: 'white',
         padding: 25
     },
     landing__welcomeText: {
-        flex: 2,
         alignItems: 'center',
         justifyContent: 'flex-start'
     },
@@ -93,25 +182,22 @@ const styles = StyleSheet.create({
 
 
     landing__dataInput: {
-        flex: 6,
         paddingVertical: 15
     },
     landing__dataInput__registrationInputs: {
-        flex: 1
+       
     },
 
 
     landing__buttonsContainer: {
-        flex: 3,
+       
     },
 
 
     landing__buttonRegistration: {
-        flex: 1,
         paddingBottom: 15
     },
     landing__buttonRegistration__button: {
-        flex: 1,
         borderColor: 'black',
         borderWidth: 2,
         padding: 10,
@@ -124,10 +210,9 @@ const styles = StyleSheet.create({
 
 
     landing__buttonLogin: {
-        flex: 1,
     },
     landing__buttonLogin__button: {
-        flex: 1,
+
         backgroundColor: '#17a2b8',
         padding: 10,
         borderRadius: 5,
@@ -135,6 +220,11 @@ const styles = StyleSheet.create({
     },
     landing__buttonLogin__text: {
         color: 'white',
+        textAlign: 'center'
+    },
+
+    landing__errorMessage: {
+        color: 'red',
         textAlign: 'center'
     }
 });
